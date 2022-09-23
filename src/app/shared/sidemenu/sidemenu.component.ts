366055f70261c6f9728d1dd5fa27a8d26c2a2e17
kadilak75 <molivastri245@gmail.com>
2022-09-23 13:44:46 +0200

    primer commit

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+    `
+      li {
+        cursor:pointer;
+      }
+    `
+  ]
+})
+export class SidemenuComponent {
+
+
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Básicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinámicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    },
+  ];
+
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Login',
+      ruta: './auth/pages/'
+    },
+    {
+      texto: 'Registro',
+      ruta: './auth/registro/'
+    }
+  ]
+
+
+}
